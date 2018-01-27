6dae506ee5781398940430b8ee7d29d56fa321a4
Omkar Lahurikar <oalahuri@mtu.edu>
2018-01-27 02:20:13 +0000

    First Commit

---
+++ app/controllers/forum_threads_controller.rb
@@ -0,0 +1,45 @@
+class ForumThreadsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_forum_thread, except: [:index, :new, :create]
+
+  def index
+    @forum_threads = ForumThread.all 
+  end
+
+  def show
+    @forum_post = ForumPost.find(params[:id])
+  end
+
+  def new
+    @forum_thread = ForumThread.new
+    @forum_thread.forum_posts.new
+  end
+
+  def create
+    @forum_thread = current_user.forum_threads.new forum_thread_params
+    @forum_thread.forum_posts.first.user_id = current_user.id
+
+    if @forum_thread.save
+      redirect_to @forum_thread
+    else
+      render action: :new
+    end
+  end
+  
+  def destroy
+    @forum_thread = ForumThread.find(params[:id])
+    @forum_thread.destroy
+    
+    redirect_to forum_threads_path
+  end
+  
+  private
+
+    def set_forum_thread
+      @forum_thread = ForumThread.find(params[:id])
+    end
+
+    def forum_thread_params
+      params.require(:forum_thread).permit(:subject, forum_posts_attributes: [:body])
+    end
+end
